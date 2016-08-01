3cb600f2a9780335ae670d162c9963cddf0280f1
Derek Tia <dtia05@gmail.com>
2016-08-01 10:26:47 -0700

    add fb service

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,7 @@
 class HomeController < ApplicationController
   def index
+    fb_service = FacebookService.new
+    puts fb_service.test
   end
 
   private
--- app/services/facebook_service.rb
+++ app/services/facebook_service.rb
@@ -1,3 +1,7 @@
+require 'http'
+
 class FacebookService
-	
+	def test
+    HTTP.get("https://github.com").to_s
+  end
 end
